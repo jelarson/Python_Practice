284bbf177bd3d2f3190d6c95cc0ffd66fa8b1e28
jelarson <jesslarson93@gmail.com>
2020-02-25 15:58:11 -0700

    added replace_function.py

---
+++ replace_function.py
@@ -0,0 +1,9 @@
+sentence = 'The quick brown fox jumps over the lazy dog'
+
+sentence = sentence.replace('quick', 'slow')
+
+print(sentence)
+
+my_string = "A very long string"
+
+print(my_string.replace("long", "short"))
