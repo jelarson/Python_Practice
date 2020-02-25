98a0b1ccca36279994b72c34ad9fb1f47492e918
jelarson <jesslarson93@gmail.com>
2020-02-25 15:43:00 -0700

    updated file

--- index_based_string_interpolation.py
+++ index_based_string_interpolation.py
@@ -3,7 +3,7 @@ age = 12
 product = 'Python eLearning course'
 from_account = 'Jordan'
 
-# greeting = "Product Purchase: {2} - Hi {0}, you are listed as {1} years old. - {3}".format(name, age, product, from_account)
+greeting = "Product Purchase: {2} - Hi {0}, you are listed as {1} years old. - {3}".format(name, age, product, from_account)
 
 greeting = f"Product Purchase: {product} - Hi {name}, you are listed as {age} years old. - {from_account}"
 
