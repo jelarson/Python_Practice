dda8a5cec01ebcdb1767e4568fc6446bce193f3d
jelarson <jesslarson93@gmail.com>
2020-02-26 11:05:45 -0700

    added mathmatical_operators.py file

---
+++ mathmatical_operators.py
@@ -0,0 +1,41 @@
+print('Addition')
+print(100 + 42)
+
+print('Subtraction')
+print(100 - 42)
+
+print('Division')
+print(100 / 42)
+print(100 / 38)
+
+print('Floor Division')
+print(100 // 42)
+print(100 // 38)
+
+print('Multiplication')
+print(100 * 42)
+
+print('Modulus')
+print(100 % 42)
+
+print('Exponents')
+print(100 ** 42)
+
+"""
+Please -> Parans ()
+Excuse -> Exponents **
+My -> Multiplication *
+Dear -> Division /
+Aunt -> Addition +
+Sally -> Subtraction -
+
+8 + 2 * 5 - (9 + 2) ** 2
+8 + 2 * 5 - 11 ** 2
+8 + 2 * 5 - 121
+8 + 10 - 121
+-103
+"""
+
+calculation = 8 + 2 * 5 - (9 + 2) ** 2
+
+print(calculation)
